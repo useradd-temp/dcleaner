3c2515a7919d4cbe45977b198ac5d6f11d081e8b
helloracoon <useradd.temp@gmail.com>
2021-05-03 12:12:58 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dcleaner',
+    version='0.0.1',
+    packages=['dcleaner'],
+    url='https://github.com/helloracoon/dcleaner',
+    license='',
+    author='TEMP USER',
+    author_email='useradd.temp@gmail.com',
+    description='docker cleaner for python'
+)
